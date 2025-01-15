38ea4ee05331d1b8a960e1310f43684b15cd834b
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-15 20:16:12 +0545

    add mergetwolists approach in mergeksortedlists

--- 23.merge-k-sorted-lists.cpp
+++ 23.merge-k-sorted-lists.cpp
@@ -27,3 +27,46 @@ public:
     return dummynode->next;
   }
 };
+class Solution // using mergetwolists apporach but it is not optimal for time complexity but is optimal for space complexity
+{
+public:
+  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+  {
+    ListNode *result = new ListNode(0);
+    ListNode *dummyhead = result;
+    while (list1 != NULL && list2 != NULL)
+    {
+      if (list2->val >= list1->val)
+      {
+        result->next = list1;
+        list1 = list1->next;
+      }
+      else
+      {
+        result->next = list2;
+        list2 = list2->next;
+      }
+      result = result->next;
+    }
+    if (list1 != NULL)
+    {
+      result->next = list1;
+    }
+    else if (list2 != NULL)
+    {
+      result->next = list2;
+    }
+    return dummyhead->next;
+  }
+  ListNode *mergeKLists(vector<ListNode *> &lists)
+  {
+    if (lists.empty())
+      return nullptr;
+    ListNode *head = lists[0];
+    for (int i = 1; i < lists.size(); i++)
+    {
+      head = mergeTwoLists(head, lists[i]);
+    }
+    return head;
+  }
+};
