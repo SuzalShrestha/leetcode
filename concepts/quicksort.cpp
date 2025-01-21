688c2108a4b5788b41706ff83c785930e5301fc7
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-21 20:04:07 +0545

    add quicksort concept

---
+++ concepts/quicksort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+int pivot(int array[], int low, int high)
+{
+  int pivotele = array[low];
+  int i = low;
+  int j = high;
+  while (i < j)
+  {
+    while (array[i] <= pivotele && i <= high)
+    {
+      i++;
+    }
+    while (array[j] > pivotele && j >= low)
+    {
+      j--;
+    }
+    if (i < j)
+    {
+      swap(array[i], array[j]);
+    }
+  }
+  swap(array[low], array[j]);
+  return j;
+}
+void quickSort(int array[], int low, int high)
+{
+  if (low < high)
+  {
+    int pe = pivot(array, low, high);
+    quickSort(array, low, pe - 1);
+    quickSort(array, pe + 1, high);
+  }
+}
+int main()
+{
+  int data[] = {-10, 100, 12, 0, 12, 2, 1};
+  int size = sizeof(data) / sizeof(data[0]);
+  quickSort(data, 0, size);
+  cout << "Sorted array in ascending order: " << endl;
+  printArray(data, size);
+}
