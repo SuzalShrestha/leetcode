124e85a1084bbbb626c9e7afa71d3149a1072501
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-21 12:02:24 +0545

    add optimal approach 234

--- 234.palindrome-linked-list.cpp
+++ 234.palindrome-linked-list.cpp
@@ -1,5 +1,5 @@
 class Solution
-{ // using stack
+{ // using stack S.C->O(n)
 public:
   bool isPalindrome(ListNode *head)
   {
@@ -20,6 +20,49 @@ public:
       stack.pop();
       temp = temp->next;
     }
+    return true;
+  }
+};
+class Solution // S.C->O(1) optimal approach
+{
+public:
+  ListNode *reverse(ListNode *head)
+  {
+    ListNode *prev = NULL;
+    ListNode *temp = head;
+    ListNode *front = NULL;
+    while (temp != NULL)
+    {
+      front = temp->next;
+      temp->next = prev;
+      prev = temp;
+      temp = front;
+    }
+    return prev;
+  }
+  bool isPalindrome(ListNode *head)
+  {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    ListNode *newhead = reverse(slow->next);
+    ListNode *first = head;
+    ListNode *second = newhead;
+    while (second != NULL)
+    {
+      if (first->val != second->val)
+      {
+        reverse(newhead);
+        return false;
+      }
+      first = first->next;
+      second = second->next;
+    }
+    reverse(newhead);
     return true;
   }
 };
