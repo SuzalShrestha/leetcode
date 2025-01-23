0aac01be575759fcef00afd07e65b48e37861d6f
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-23 21:58:14 +0545

    add towerofhanoi concept

---
+++ concepts/towerofhanoi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void towerofhanoi(int n, char from_rod, char aux_rod, char to_rod)
+{
+  if (n == 0)
+    return;
+  towerofhanoi(n - 1, from_rod, aux_rod, to_rod);
+  cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
+  towerofhanoi(n - 1, aux_rod, to_rod, from_rod);
+}
+int main()
+{
+  int N = 3;
+  towerofhanoi(N, 'A', 'C', 'B');
+  return 0;
+}
