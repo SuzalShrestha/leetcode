9455f2be596b1a72f5b00150a080a681a7a49906
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-24 22:08:26 +0545

    add new approach for 103

--- 103.binary-tree-zigzag-level-order-traversal.cpp
+++ 103.binary-tree-zigzag-level-order-traversal.cpp
@@ -38,3 +38,35 @@ public:
     return ans;
   }
 };
+class Solution
+{
+public:
+  vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+  {
+    vector<vector<int>> ans;
+    if (root == NULL)
+      return ans;
+    queue<TreeNode *> q;
+    q.push(root);
+    bool isLeftToRight = true;
+    while (!q.empty())
+    {
+      int size = q.size();
+      vector<int> level(size);
+      for (int i = 0; i < size; i++)
+      {
+        TreeNode *node = q.front();
+        q.pop();
+        int index = (isLeftToRight) ? i : (size - 1 - i);
+        level[index] = node->val;
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
+      isLeftToRight = !isLeftToRight;
+      ans.push_back(level);
+    }
+    return ans;
+  }
+};
