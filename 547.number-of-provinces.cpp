26e972e6a5833050f3a57666c7654db58a360a56
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-28 16:07:20 +0545

    add conversion of adjacency matrix to list

--- 547.number-of-provinces.cpp
+++ 547.number-of-provinces.cpp
@@ -28,3 +28,15 @@ public:
     return province;
   }
 };
+// Note: You can change adjacency matrix into adjacency list using loops and then perform the algorithm if necessary
+// for (int i = 0; i < N; i++)
+// {
+//   for (int j = 0; j < N; j++)
+//   {
+//     if (isConnected[i][j] == 1 && i != j)
+//     {
+//       adj[i].push_back(j);
+//       adj[j].push_back(i);
+//     }
+//   }
+// }
