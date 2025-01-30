982ca00c8fffc11f2638e05774786a8af0775f5d
sujalshrestha <sujalshresthawork@gmail.com>
2025-01-30 14:59:06 +0545

    add dijkstra algorithm concept

---
+++ concepts/dijkstra-algorithm.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+  // Function to find the shortest distance of all the vertices
+  // from the source vertex src.
+  vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src)
+  {
+    // implementation of min heap priority queue
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    int n = adj.size();
+    vector<int> dist(n);
+    for (int i = 0; i < n; i++)
+      dist[i] = 1e9;
+    dist[src] = 0;
+    pq.push({0, src});
+    while (!pq.empty())
+    {
+      int distance = pq.top().first;
+      int node = pq.top().second;
+      pq.pop();
+      for (auto it : adj[node])
+      {
+        int edgeWeight = it.second;
+        int neighbour = it.first;
+        if (distance + edgeWeight < dist[neighbour])
+        {
+          dist[neighbour] = distance + edgeWeight;
+          pq.push({dist[neighbour], neighbour});
+        }
+      }
+    }
+    return dist;
+  }
+};
