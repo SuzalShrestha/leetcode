c1143266efe50efff6804bfdf5846697201baba1
sujalshrestha <sujalshresthawork@gmail.com>
2025-02-05 16:09:41 +0545

    add 547 using disjoint sets

--- 547.number-of-provinces.cpp
+++ 547.number-of-provinces.cpp
@@ -40,3 +40,66 @@ public:
 //     }
 //   }
 // }
+// using disjointset
+class DisjointSet
+{
+public:
+  vector<int> rank, parent, size;
+  DisjointSet(int n)
+  {
+    rank.resize(n + 1, 0);
+    parent.resize(n + 1);
+    size.resize(n + 1);
+    for (int i = 0; i <= n; i++)
+      parent[i] = i;
+  }
+  int findUltimateParent(int node) // retrieve at constant time
+  {
+    if (node == parent[node])
+      return node;
+    return parent[node] = findUltimateParent(parent[node]);
+  }
+  void unionBySize(int u, int v)
+  {
+    int ulp_u = findUltimateParent(u);
+    int ulp_v = findUltimateParent(v);
+    if (ulp_u == ulp_v)
+      return;
+    if (size[ulp_u] < size[ulp_v])
+    {
+      parent[ulp_u] = ulp_v;
+      size[ulp_v] += size[ulp_u];
+    }
+    else
+    {
+      parent[ulp_v] = ulp_u;
+      size[ulp_u] += size[ulp_v];
+    }
+  }
+};
+class Solution
+{
+public:
+  int findCircleNum(vector<vector<int>> &isConnected)
+  {
+    int n = isConnected.size();
+    DisjointSet ds(n);
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (isConnected[i][j] == 1)
+        {
+          ds.unionBySize(i, j);
+        }
+      }
+    }
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (ds.findUltimateParent(i) == i)
+        cnt++;
+    }
+    return cnt;
+  }
+};
