9673da870d01c3023f6238f124332668bad109f2
sujalshrestha <sujalshresthawork@gmail.com>
2025-02-06 21:02:53 +0545

    add 26.remove-duplicates-from-sorted-array

---
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution
+{ // two pointer approach
+public:
+  int removeDuplicates(vector<int> &nums)
+  {
+    if (nums.empty())
+      return 0;
+    int n = nums.size();
+    int k = 1;
+    for (int i = 1; i < n; i++)
+    {
+      if (nums[i] != nums[i - 1])
+      {
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+    return k;
+  }
+};
