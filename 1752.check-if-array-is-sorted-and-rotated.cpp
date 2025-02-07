bc51329e2d772a12c8ababcc8cc122911da05f25
Sujal Shrestha <84851820+SuzalShrestha@users.noreply.github.com>
2025-02-07 08:49:28 +0545

    add bug fix 1752.check-if-array-is-sorted-and-rotated.cpp

--- 1752.check-if-array-is-sorted-and-rotated.cpp
+++ 1752.check-if-array-is-sorted-and-rotated.cpp
@@ -9,7 +9,7 @@ public:
       return true;
     for (int i = 1; i < 2 * n; i++)
     {
-      if (nums[i % n] > nums[(i - 1) % n])
+      if (nums[i % n] >= nums[(i - 1) % n])
         cnt++;
       else
         cnt = 1;
