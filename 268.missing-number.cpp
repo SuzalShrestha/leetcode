4b0ef48596854b6262e23e34c891829e8a320a60
sujalshrestha <sujalshresthawork@gmail.com>
2025-02-08 13:25:39 +0545

    add optimal xor and sum solutions

--- 268.missing-number.cpp
+++ 268.missing-number.cpp
@@ -42,3 +42,52 @@ public:
     return n;
   }
 };
+class Solution
+{ // optimal solution using sum of n natural numbers formula
+public:
+  int missingNumber(vector<int> &nums)
+  {
+    int n = nums.size();
+    int sum = (n * (n + 1)) / 2;
+    int asum = 0;
+    for (auto it : nums)
+      asum += it;
+    return sum - asum;
+  }
+};
+class Solution
+{ // optimal xor solution better than sum for large integer eg:10^5
+public:
+  int missingNumber(vector<int> &nums)
+  {
+    int n = nums.size();
+    int xor1 = 0;
+    int xor2 = 0;
+    for (int i = 0; i <= n; i++)
+    {
+      xor1 = xor1 ^ i;
+    }
+    for (int i = 0; i < n; i++)
+    {
+      xor2 = xor2 ^ nums[i];
+    }
+    return xor1 ^ xor2;
+  }
+};
+class Solution
+{ // more optimal solution
+public:
+  int missingNumber(vector<int> &nums)
+  {
+    int n = nums.size();
+    int xor1 = 0;
+    int xor2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+      xor1 = xor1 ^ i;
+      xor2 = xor2 ^ nums[i];
+    }
+    xor1 = xor1 ^ n;
+    return xor1 ^ xor2;
+  }
+};
