7d906895ce168b006e190d4b4e6457b8a0f6315e
sujalshrestha <sujalshresthawork@gmail.com>
2025-02-09 18:14:24 +0545

    add bruteforce to 75

--- 75.sort-colors.cpp
+++ 75.sort-colors.cpp
@@ -32,3 +32,33 @@ public:
     return quicksort(nums, 0, nums.size() - 1);
   }
 };
+class Solution
+{ // bruteforce solution as nums contain 0,1 and 2 only.
+public:
+  void sortColors(vector<int> &nums)
+  {
+    int n = nums.size();
+    int zeros = 0, ones = 0, twos = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (nums[i] == 0)
+        zeros++;
+      if (nums[i] == 1)
+        ones++;
+      else
+        twos++;
+    }
+    for (int i = 0; i < zeros; i++)
+    {
+      nums[i] = 0;
+    }
+    for (int i = zeros; i < zeros + ones; i++)
+    {
+      nums[i] = 1;
+    }
+    for (int i = zeros + ones; i < n; i++)
+    {
+      nums[i] = 2;
+    }
+  }
+};
