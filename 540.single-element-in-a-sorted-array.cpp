b867597d87896280f318c02a8c7ebb7e7eeba0dd
sujalshrestha <sujalshresthawork@gmail.com>
2025-02-17 23:47:37 +0545

    add optimal solution to 540

--- 540.single-element-in-a-sorted-array.cpp
+++ 540.single-element-in-a-sorted-array.cpp
@@ -57,3 +57,35 @@ public:
     return nums[low];
   }
 };
+class Solution
+{ // optimal solution
+public:
+  int singleNonDuplicate(vector<int> &nums)
+  {
+    int n = nums.size();
+    if (n == 1)
+      return nums[0];
+    if (nums[0] != nums[1])
+      return nums[0];
+    if (nums[n - 1] != nums[n - 2])
+      return nums[n - 1];
+    int high = n - 2, low = 1;
+    while (low <= high)
+    {
+      int mid = low + (high - low) / 2;
+
+      if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        return nums[mid];
+
+      if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+          (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+
+        low = mid + 1;
+
+      else
+        high = mid - 1;
+    }
+    // dummy index
+    return -1;
+  }
+};
