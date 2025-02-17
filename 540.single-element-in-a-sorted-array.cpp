80755bb56d27f7f0cd6153b97b82d70867eb306b
sujalshrestha <sujalshresthawork@gmail.com>
2025-02-17 23:22:36 +0545

    add 540 optimal and brute solution

---
+++ 540.single-element-in-a-sorted-array.cpp
@@ -0,0 +1,59 @@
+class Solution
+{ // bruteforce solution O(n)
+public:
+  int singleNonDuplicate(vector<int> &nums)
+  {
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+      if (i == 0)
+      {
+        if (nums[i + 1] != nums[i])
+          return nums[i];
+      }
+      else if (i == n - 1)
+      {
+        if (nums[i] != nums[i - 1])
+          return nums[i];
+      }
+      else if (nums[i - 1] != nums[i] && nums[i] != nums[i + 1])
+      {
+        return nums[i];
+      }
+    }
+    return -1;
+  }
+};
+class Solution
+{ // better solution using xor O(n)
+public:
+  int singleNonDuplicate(vector<int> &nums)
+  {
+    int xor1 = 0;
+    for (auto i : nums)
+      xor1 ^= i;
+    return xor1;
+  }
+};
+class Solution
+{ // optimal solution O(logn) using single element project of even and odd index
+public:
+  int singleNonDuplicate(vector<int> &nums)
+  {
+    int n = nums.size();
+    int high = n - 1, low = 0;
+    while (low < high)
+    {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == nums[mid ^ 1])
+      {
+        low = mid + 1;
+      }
+      else
+      {
+        high = mid;
+      }
+    }
+    return nums[low];
+  }
+};
