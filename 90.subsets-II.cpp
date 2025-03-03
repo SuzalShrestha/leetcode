f5050f617269859e96c3ae22233174819f9f6c2d
sujalshrestha <sujalshresthawork@gmail.com>
2025-03-03 20:53:13 +0545

    add 90 alternative solution

--- 90.subsets-II.cpp
+++ 90.subsets-II.cpp
@@ -1,5 +1,5 @@
 class Solution
-{
+{ // only works if sorted
 public:
   void dfs(int i, vector<int> &nums, vector<vector<int>> &res,
            vector<int> &curr)
@@ -15,6 +15,31 @@ public:
     while (i + 1 < nums.size() && nums[i] == nums[i + 1])
       i++;
     dfs(i + 1, nums, res, curr);
+  }
+  vector<vector<int>> subsetsWithDup(vector<int> &nums)
+  {
+    vector<vector<int>> res;
+    vector<int> curr;
+    sort(nums.begin(), nums.end());
+    dfs(0, nums, res, curr);
+    return res;
+  }
+};
+class Solution
+{
+public:
+  void dfs(int i, vector<int> &nums, vector<vector<int>> &res,
+           vector<int> &curr)
+  {
+    res.push_back(curr);
+    for (int j = i; j < nums.size(); j++)
+    {
+      if (j != i && nums[j] == nums[j - 1])
+        continue;
+      curr.push_back(nums[j]);
+      dfs(j + 1, nums, res, curr);
+      curr.pop_back();
+    }
   }
   vector<vector<int>> subsetsWithDup(vector<int> &nums)
   {
