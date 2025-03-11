8f68895eaabfaf79038249b6f9a29ac456b52596
sujalshrestha <sujalshresthawork@gmail.com>
2025-03-11 19:45:03 +0545

    add 1358 optimal solution

--- 1358.number-of-substrings-containing-all-three-characters.cpp
+++ 1358.number-of-substrings-containing-all-three-characters.cpp
@@ -1,5 +1,6 @@
 class Solution
-{
+{ // better solution
+  // not efficient as lookup time and erasing is log(n)
 public:
   int numberOfSubstrings(string s)
   {
@@ -16,6 +17,29 @@ public:
         mp[s[l]]--;
         if (mp[s[l]] == 0)
           mp.erase(s[l]);
+        l++;
+      }
+    }
+    return res;
+  }
+};
+class Solution
+{ // optimal solution
+  // array lookup time O(1)
+public:
+  int numberOfSubstrings(string s)
+  {
+    int n = s.size();
+    int l = 0;
+    int res = 0;
+    vector<int> freq(3, 0);
+    for (int r = 0; r < s.size(); r++)
+    {
+      freq[s[r] - 'a']++;
+      while (freq[0] && freq[1] && freq[2])
+      {
+        res += n - r;
+        freq[s[l] - 'a']--;
         l++;
       }
     }
