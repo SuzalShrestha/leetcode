2ea48bfc20265207e055cf4461e2cbee176077dc
sujalshrestha <sujalshresthawork@gmail.com>
2025-03-14 17:42:53 +0545

    add 875 using binary search

---
+++ 875.koko-eating-bananas.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+  int isValid(vector<int> &piles, int h, int num)
+  {
+    long long sum = 0;
+    for (int p : piles)
+    {
+      sum += (p + num - 1) / num;
+    }
+    return sum <= h;
+  }
+  int minEatingSpeed(vector<int> &piles, int h)
+  {
+    long long sum = 0;
+    for (int p : piles)
+    {
+      sum += p;
+    }
+    int l = 1, r = *max_element(piles.begin(), piles.end());
+    while (l <= r)
+    {
+      int mid = (l + r) / 2;
+      if (isValid(piles, h, mid))
+      {
+        r = mid - 1;
+      }
+      else
+        l = mid + 1;
+    }
+    return l;
+  }
+};
