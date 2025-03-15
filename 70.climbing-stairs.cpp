5f357153a887136e9ff5364ab122837a42f49dcf
sujalshrestha <sujalshresthawork@gmail.com>
2025-03-15 23:40:48 +0545

    add 70 dp solution

---
+++ 70.climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution
+{ // dp solution
+public:
+  int climbStairs(int n)
+  {
+    if (n == 1)
+      return 1;
+    if (n == 2)
+      return 2;
+    vector<int> mem(n, -1);
+    mem[0] = 1;
+    mem[1] = 2;
+    int sum = 0;
+    for (int i = 2; i < n; i++)
+    {
+      mem[i] = mem[i - 1] + mem[i - 2];
+    }
+    return mem[n - 1];
+  }
+};
