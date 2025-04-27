6084300c67a73a5f92490d8cd889f6fb0def8132
sujalshrestha <sujalshresthawork@gmail.com>
2025-04-27 21:00:10 +0545

    add 3392

---
+++ 3392.count-subarrays-of-length-three-with-a-condition.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+  int countSubarrays(vector<int> &nums)
+  {
+    int res = 0;
+    for (int i = 2; i < nums.size(); i++)
+    {
+      if (((nums[i - 2] + nums[i]) * 2) == (nums[i - 1]))
+        res++;
+    }
+    return res;
+  }
+};
