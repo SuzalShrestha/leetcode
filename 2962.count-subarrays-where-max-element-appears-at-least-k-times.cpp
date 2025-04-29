ed0d70129150cf3af62b173623cfd0c8065044ed
sujalshrestha <sujalshresthawork@gmail.com>
2025-04-29 16:04:30 +0545

    add another approach for 2962

--- 2962.count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962.count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,5 +1,5 @@
 class Solution
-{
+{ // sliding window
 public:
   long long countSubarrays(vector<int> &nums, int k)
   {
@@ -19,6 +19,31 @@ public:
         l++;
       }
       r++;
+    }
+    return res;
+  }
+};
+class Solution
+{ // non-sliding window approach
+public:
+  long long countSubarrays(vector<int> &nums, int k)
+  {
+    int n = nums.size();
+    int maxele = *max_element(nums.begin(), nums.end());
+    vector<int> maxindices;
+    long long res = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (nums[i] == maxele)
+      {
+        maxindices.push_back(i);
+      }
+      int size = maxindices.size();
+      if (size >= k)
+      {
+        int lastindx = maxindices[size - k];
+        res += lastindx + 1;
+      }
     }
     return res;
   }
