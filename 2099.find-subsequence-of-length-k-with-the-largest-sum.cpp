1276aac2a9c86b2cdd3bd7a22c15acfc81927d84
sujalshrestha <sujalshresthawork@gmail.com>
2025-06-28 23:33:15 +0545

    optimal add 2099

--- 2099.find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099.find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -25,3 +25,38 @@ public:
     return result;
   }
 };
+class Solution
+{
+public:
+  vector<int> maxSubsequence(vector<int> &nums, int k)
+  {
+    int n = nums.size();
+    if (k == n)
+    {
+      return nums;
+    }
+    vector<int> temp(nums);
+    nth_element(begin(temp), begin(temp) + k - 1, end(temp),
+                greater<int>());
+    int kthLargest = temp[k - 1];
+    int countKthLargest = count(begin(temp), begin(temp) + k, kthLargest);
+    vector<int> result;
+    for (int &num : nums)
+    {
+      if (num > kthLargest)
+      {
+        result.push_back(num);
+      }
+      else if (num == kthLargest && countKthLargest > 0)
+      {
+        result.push_back(num);
+        countKthLargest--;
+      }
+      if (result.size() == k)
+      {
+        break;
+      }
+    }
+    return result;
+  }
+};
